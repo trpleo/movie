939957499eba262485c2527decc1bcbba0be69df
ipapp-bc <ipapp@blockchain.com>
2023-04-25 05:26:52 +0100

    feat: add RESTful header control

--- src/main/kotlin/jpm/movie/core/HttpApiImpl.kt
+++ src/main/kotlin/jpm/movie/core/HttpApiImpl.kt
@@ -1,11 +1,14 @@
 package jpm.movie.core
 
+import arrow.core.EitherNel
+import aws.smithy.kotlin.runtime.http.HttpBody.Empty.contentLength
 import com.google.inject.Inject
 import com.google.inject.Singleton
 import io.ktor.server.application.call
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.netty.NettyApplicationEngine
+import io.ktor.server.response.header
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
@@ -14,6 +17,18 @@ import java.util.logging.Logger
 import jpm.movie.Log
 import jpm.movie.model.Codecs
 import jpm.movie.model.RawRequest
+import io.ktor.server.application.*
+import io.ktor.http.*
+import io.ktor.server.request.header
+//import io.ktor.server.plugins.autohead.*
+//import io.ktor.server.plugins.partialcontent.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.io.File
+import jpm.movie.model.MovieResponse
+import jpm.movie.model.QueryResult
+import jpm.movie.model.RequestFailure
+import jpm.movie.model.ValidationError
 
 /**
  * Embedded Http server, that provides the APIs the service can be called.
@@ -36,19 +51,29 @@ class HttpApiImpl @Inject constructor(
         applicationEngine = embeddedServer(Netty, port = httpConfig.port, host = httpConfig.host) {
             routing {
                 get("/healthcheck") {
-                    call.respondText(Instant.now().toEpochMilli().toString())
+                    call.respondText(Instant.now().toEpochMilli().toString(), ContentType.Text.Plain, HttpStatusCode.OK)
                 }
                 get("/movies") {
-                    val years = call.request.queryParameters["years"]?.split(",")?.toSet() ?: emptySet()
-                    val movieNames = call.request.queryParameters["titles"]?.split(",")?.toSet() ?: emptySet()
-                    val castMember = call.request.queryParameters["cast"]?.split(",")?.toSet() ?: emptySet()
-                    val genres = call.request.queryParameters["genres"]?.split(",")?.toSet() ?: emptySet()
+                    if (call.request.header(HttpHeaders.Accept) != "application/json")
+                    // Indicates that the response could not be processed by the client.
+                        call.response.status(HttpStatusCode.NotAcceptable)
+                    else if (call.request.header(HttpHeaders.ContentType) != "application/json") {
+                        // Indicates the content type that is used in the body of the request is not supported
+                        call.response.status(HttpStatusCode.BadRequest)
+                    } else {
+                        val years = call.request.queryParameters["years"]?.split(",")?.toSet() ?: emptySet()
+                        val movieNames = call.request.queryParameters["titles"]?.split(",")?.toSet() ?: emptySet()
+                        val castMember = call.request.queryParameters["cast"]?.split(",")?.toSet() ?: emptySet()
+                        val genres = call.request.queryParameters["genres"]?.split(",")?.toSet() ?: emptySet()
 
-                    logger.info("years: [$years]; movies: [$movieNames]; cast: [$castMember]; genres: [$genres]")
+                        logger.info("years: [$years]; movies: [$movieNames]; cast: [$castMember]; genres: [$genres]")
 
-                    queryService.queryMovies(RawRequest(years, movieNames, castMember, genres))
-                        .also { logger.info { "Response: [$it]" } }
-                        .let { call.respondText(it.toProtoJson()) }
+                        queryService.queryMovies(RawRequest(years, movieNames, castMember, genres))
+                            .also { logger.info { "Response: [$it]" } }
+                            .let {
+                                call.respondText(it.toProtoJson(), ContentType.Application.Json, generateStatusCode(it))
+                            }
+                    }
                 }
             }
         }.start(wait = true)
@@ -57,4 +82,21 @@ class HttpApiImpl @Inject constructor(
     override fun stop() {
         applicationEngine?.stop()
     }
+}
+
+private fun identifyRequestFailureStatusCode(failure: RequestFailure): HttpStatusCode {
+    // based on the different Errors, the status code can be controlled through this function
+    return if (failure.errors.size == 1 && failure.errors.first() is ValidationError) {
+        HttpStatusCode.BadRequest
+    } else {
+        HttpStatusCode.InternalServerError
+    }
+}
+
+private fun generateStatusCode(response: MovieResponse) = when (response) {
+    is RequestFailure ->
+        identifyRequestFailureStatusCode(response)
+
+    is QueryResult ->
+        if (response.movies.isEmpty()) HttpStatusCode.NotFound else HttpStatusCode.OK
 }
